f86bf8702ac40146bd0fa1a7eda947fbb085c42c
DrissArezki <driss.arezki5@gmail.com>
2023-03-21 14:29:08 -0700

    0x02-functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,14 +12,14 @@ int main(void)
 	for (c = 0; c < 50; c++)
 	{
 		sum = f1 + f2;
-		printf("%lu ", sum);
+		printf("%lu", sum);
 		f1 = f2;
 		f2 = sum;
 
 		if (c == 49)
 			printf("\n");
 		else
-			printf(",");
+			printf(", ");
 	}
 	return (0);
 }
