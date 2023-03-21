a38947eec1cda1b539e3524388e5bb7e3631d28f
DrissArezki <driss.arezki5@gmail.com>
2023-03-21 07:05:09 -0700

    0x02-fucntions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int a;
 	for (a=97, a<=122, a++)
-	(
+	{
 	       _putchar(a);
-	)
+	}
 	_putchar('\n');
 }	
 
