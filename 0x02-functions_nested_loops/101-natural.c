6fa329d1dca6d4a8e41040a6ce1313341796ada4
DrissArezki <driss.arezki5@gmail.com>
2023-03-21 14:14:48 -0700

    0x02-functions, nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - lists the sum of all the multiples of 3 and 5 below 1024
+ */
+int main(void)
+{
+	int i;
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum = sum + i;
+	}
+	printf ("%d\n", sum);
+	return (0);
+}
