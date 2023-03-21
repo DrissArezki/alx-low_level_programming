1b9654f02253b33bf8a1f5b98aab83d0a9f67932
DrissArezki <driss.arezki5@gmail.com>
2023-03-21 07:37:39 -0700

    0x02-fucntions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,12 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-	int i=0;
-	while (i<10)
+	int i;
+	i = 0;
+	while(i < 10)
 	{
 		ch='a';
-		while (ch <='z')
+		while(ch <='z')
 		{
 			_putchar(ch);
 			ch++;
