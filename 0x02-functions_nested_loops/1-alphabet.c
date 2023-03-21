bfc6585e264f7e382a9d76277d6c2f178d2a2cf7
DrissArezki <driss.arezki5@gmail.com>
2023-03-21 07:02:41 -0700

    0x02-fucntions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	int a;
+	for (a=97, a<=122, a++)
+	{
+	       _putchar(a);
+	}
+	_putchar('\n');
+}	
+
