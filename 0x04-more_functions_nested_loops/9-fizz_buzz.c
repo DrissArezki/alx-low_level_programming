39a674d2f20341bff13172bb1fb1fe7e14712685
DrissArezki <driss.arezki5@gmail.com>
2023-03-23 16:48:33 -0700

    0x04-More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,25 +8,21 @@ int main(void)
 {
 	int i;
 
-	for (i = 1, i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		}
-		else if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
-		}
-		else if (i == 1)
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
 		{
 			printf("%d", i);
-		}
-		else
+		} else
 		{
 			printf(" %d", i);
 		}
@@ -35,3 +31,4 @@ int main(void)
 
 	return (0);
 }
+
