edbc1b08c18e7b076e21c5ff3345739392bb3ea2
DrissArezki <driss.arezki5@gmail.com>
2023-03-23 05:50:12 -0700

    0x04-More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+ * print_line - draws a straight line
+ * @n: number of times the char _ should be printed
+ * Return: void
+ */
+void print_line(int n);
+{
+	int n, i;
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	_putchar('n\');
+}
