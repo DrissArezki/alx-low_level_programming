936ca92b5858abb334a7474a2e41193f01c66b4c
DrissArezki <driss.arezki5@gmail.com>
2023-03-23 17:17:51 -0700

    0x04-More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the largest prime number of 612852475143
+ * Return: void
+ */
+int main(void)
+{
+
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		n *= -1;
+		x = n;
+		_putchar('-');
+	}
+	x = x / 10;
+	if (x != 0)
+		print_number(x);
+	_putchar(n % 10 + '0');
+}
+
