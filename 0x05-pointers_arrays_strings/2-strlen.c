2dcd2b809c19d62d5fb01d0318b99003b59ccb55
DrissArezki <driss.arezki5@gmail.com>
2023-03-27 17:25:36 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen(char *s) - returns the length of a string
+ * @s: the string to use
+ * Return: length of the string (Success)
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	if (*s != '0')
+	{
+		length++;
+		s++;
+	}
+	else
+	{
+		length = 0;
+	}
+	return (length);
+}
