913a4ae2fe4d333b6862f4c850f11ca1a38d9cb9
DrissArezki <driss.arezki5@gmail.com>
2023-03-30 07:08:45 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,20 +4,20 @@
  * @dest: string to concatenate
  * @src: string to concatenate
  * @n: number of bytes from src
- * Return: string
+ * Return: concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len_dest = 0;
-	int len_src = 0;
+	int lendest = 0;
+	int lensrc = 0;
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		len_dest++;
-	for (i = 0; src[i] != '\0'; i++)
-		len_src++;
-	for (i = 0; i < n; i++)
-		dest[len_dest + i] = src[i];
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		lendest++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		lensrc++;
+	for (i = 0 ; i < n ; i++)
+		dest[lendest + i] = src[i];
 	return (dest);
 }
