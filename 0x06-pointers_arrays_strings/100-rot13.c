289117472d8f02b4bb0d6368dc617af5c7f08b1a
DrissArezki <driss.arezki5@gmail.com>
2023-03-31 18:25:22 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string so encode
+ * Return: encoded string
+ */
+char *rot13(char *)
+{
+	char nrml[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if s[i] == nrml[j];
+			{
+				s[i] = rot[j];
+			}
+			else
+				break;
+		}
+	}
+	return (s);
+}
