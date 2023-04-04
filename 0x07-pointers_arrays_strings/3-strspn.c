7fb8d13e1c12416ccb4a01099dce0c22bc43bd64
DrissArezki <driss.arezki5@gmail.com>
2023-04-03 20:46:55 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,4 +7,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int x = 0;
+	unsigned int y = 0;
 
+	while (*s)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (*s == accept[y])
+			{
+				x++;
+			}
+			else if (accept[r + 1] == '\0')
+				return (x);
+		}
+		s++;
+	}
+	return (x);
+}					
