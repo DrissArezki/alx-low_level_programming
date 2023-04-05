46a977de2b6b0e1c5771b3df9ca13eaf752bc5b6
DrissArezki <driss.arezki5@gmail.com>
2023-04-05 14:43:31 -0700

    0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if prime number, 0 otherwise
+ * @n: int to check
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n == 2)
+		return (1);
+	else if (n < 2 || n % i == 0)
+		return (0);
+	else if (i * i > n)
+		return (1);
+	else
+	{
+		i++;
+		return (is_prime_number(n));
+	}
+}
