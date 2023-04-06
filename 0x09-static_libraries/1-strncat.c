f40884e4e93106956e29e7a239c8606aa7dce323
DrissArezki <driss.arezki5@gmail.com>
2023-04-06 10:36:25 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: string to concatenate
+ * @src: string to concatenate
+ * @n: number of bytes from src
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
