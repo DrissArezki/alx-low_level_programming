96f41929de7ade686c6c81a7da64f9b74fd8cb0f
DrissArezki <driss.arezki5@gmail.com>
2023-04-06 10:17:07 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,26 +7,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '\0')
-		return (haystack);
-
-	/*char *h = haystack;
-	char *n = needle;*/
-
-	while (*haystack != '\0')
+	for (; *haystack != '\0'; haystack++)
 	{
 		char *h = haystack;
 		char *n = needle;
 
-		while (*h == *n && *n !='\0')
+		while (*h == *n && *n != '\0')
 		{
 			h++;
 			n++;
 		}
 		if (*n == '\0')
 			return (haystack);
-
-		haystack++;
 	}
 	return (0);
 }
