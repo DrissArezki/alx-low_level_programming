e62dce4d3d3e7d6e31c746db6ac4d157b5ad4c5e
DrissArezki <driss.arezki5@gmail.com>
2023-04-10 13:45:51 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: arguments counts
+ * @argv: arguments vector
+ * @i: int of incrementation
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%d\n", argv[i]);
+	return (0);
+}
