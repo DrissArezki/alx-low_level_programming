5718393bdfbf20dd380d9d80459a0f215805e687
DrissArezki <driss.arezki5@gmail.com>
2023-04-10 14:55:24 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "main.h"
 /**
  * main - adds positive nuhmbers
  * @argv: arguments vector
@@ -9,27 +11,30 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	int i, j;
+	int i;
 	char *s;
 
 	if (argc > 1)
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
+			s = argv[];
 			for (j = 0; s[j] != '\0'; j++)
 			{
-			if (s[j] < 48 || s[j] > 57)
-			{
-				printf("Error\n");
-				return (1);
+				if (s[j] < 48 || s[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			}
-		}
-	}
-	for (i = 0; i < argc; i++)
-	{
-		sum = sum + atoi(argv[i]);
+			
+			sum = sum + atoi(s);
+			s++;
 	}
 	printf("%d\n", sum);
+	else
+	{
+		printf("0\n");
+	}
 	return (0);
 }
