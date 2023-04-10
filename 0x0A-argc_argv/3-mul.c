1b6c3e4c3958a628aa7ae4c2e3021430b67a8373
DrissArezki <driss.arezki5@gmail.com>
2023-04-10 14:07:37 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,28 +7,28 @@
  */
 int _atoi(char *s)
 {
-        int sign = 1;
-        unsigned int i = 0;
-        int check = 0;
+	int sign = 1;
+	unsigned int i = 0;
+	int check = 0;
 
-        while (*s != '\0')
-        {
-                if (*s == '-' && !check)
-                {
-                        sign *= -1;
-                }
-                else if (*s >= '0' && *s <= '9')
-                {
-                        check = 1;
-                        i = i * 10 + (*s - '0');
-                }
-                else if (check)
-                {
-                break;
-                }
-        s++;
-        }
-        return (i * sign);
+	while (*s != '\0')
+	{
+		if (*s == '-' && !check)
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			check = 1;
+			i = i * 10 + (*s - '0');
+		}
+		else if (check)
+		{
+			break;
+		}
+		s++;
+	}
+	return (i * sign);
 }
 
 
