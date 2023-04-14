6edb1752aeefbd1d533333d1bec245b4a3f57c02
DrissArezki <driss.arezki5@gmail.com>
2023-04-13 19:49:12 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,19 +8,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i;
+	unsigned int i;
 	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	p = malloc(nmemb * size);
-
 	if (p == NULL)
 		return (NULL);
-
 	for (i = 0; i < nmemb * size; i++)
 		p[i] = 0;
-
 	return (p);
 }
