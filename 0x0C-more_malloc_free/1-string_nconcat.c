d15e3d2eefa98bc00627750df88a64915a545c60
DrissArezki <driss.arezki5@gmail.com>
2023-04-13 19:27:32 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,44 +5,48 @@
  * @s1: first string
  * @s2: second string
  * @n: bytes to add for the concat
- * Return : pointer
+ * Return: pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len;
-	unsigned int s2_len;
-	unsigned int total_len;
+	unsigned int s1_len = 0;
+	unsigned int s2_len = 0;
+	unsigned int i = 0, j = 0;
 	char *str;
-	char *temp;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (s1 != NULL)
+	{
+		while (s1[s1_len] != '\0')
+			s1_len++;
+	}
 
-	while (s1[s1_len] != '\0')
-		s1_len++;
-
-	while (s2[s2_len] != '\0')
-		s2_len++;
+	if (s2 != NULL)
+	{
+		while (s2[s2_len] != '\0')
+			s2_len++;
+	}
 
 	if (n >= s2_len)
 		n = s2_len;
 
-	total_len = s1_len + n;
-
-	str = (char *)malloc(total_len + 1);
+	str = malloc(sizeof(char) * (s1_len + n + 1));
 
 	if (str == NULL)
 		return (NULL);
 
-	temp = str;
-	while (*s1 != '\0')
-		*temp++ = *s1++;
-	while (n-- > 0)
-		*temp++ = *s2++;
+	while (i < s1_len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
 
-	*temp = '\0';
+	while (n < s2_len && i < (s1_len +n))
+		str[i++] = s2[j++];
+
+	while (n >= s2_len && i < (s1_len + s2_len))
+		str[i++] = s2[j++];
+
+	str[i] = '\0';
 
 	return (str);
 }
