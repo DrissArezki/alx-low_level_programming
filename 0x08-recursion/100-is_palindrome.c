27dcf090d8f04571194f2819de06886a9c89a2df
DrissArezki <driss.arezki5@gmail.com>
2023-06-07 13:59:34 -0700

    0x08-recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * is_palindrome_recursive - recursive fonction helping the main
+ * @s: char to check if palindrome
+ * @debut: start index
+ * @length: length of the string
+ * Return: test
+ */
+
+int is_palindrome_recursive(char *s, int debut, int length)
+{
+	if (debut >= length)
+		return (1);
+	if (s[debut] == s[length])
+		return (is_palindrome_recursive(s, debut + 1, length - 1));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - main function
+ * @s: char to check
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	int debut = 0;
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	length--;
+
+	return (is_palindrome_recursive(s, debut, length));
+}
