7c277bd9f0f3d3110e910b42d8201c5869affb77
DrissArezki <driss.arezki5@gmail.com>
2024-02-01 13:47:24 -0800

    0x12-single_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,19 +13,19 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *n;
 	unsigned int len = 0;
-	
+
 	while (str[len])
 		len++;
-	
+
 	n = malloc(sizeof(list_t));
-	
+
 	if (!n)
 		return (NULL);
-	
+
 	n->str = strdup(str);
 	n->len = len;
 	n->next = (*head);
 	(*head) = n;
-	
+
 	return (*head);
 }
