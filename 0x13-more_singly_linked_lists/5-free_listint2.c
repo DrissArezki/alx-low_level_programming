7c6d165a0e6e71db5c8fb784ab481306b4e61355
DrissArezki <driss.arezki5@gmail.com>
2024-02-05 12:33:32 -0800

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: argument
+ * Return: free list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = head;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		next = temp->next;
+		free(temp);
+		temp = next;
+	}
+
+	*head = NULL;
+}
