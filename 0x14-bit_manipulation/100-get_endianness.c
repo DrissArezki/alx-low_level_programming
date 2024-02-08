665fbfbfb27ed1aebcb9ef65201d2bb174b6c796
DrissArezki <driss.arezki5@gmail.com>
2024-02-08 14:51:48 -0800

    0x14-bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * get_endianness - endian
- * Return: endian
+ * get_endianness - endiaaaan
+ * Return 1 or 0, endian
  */
 int get_endianness(void)
 {
 	unsigned int number = 1;
 
-	if (*((char *)&number) == 1)
+	if (*((char*)&number) == 1)
 		return (1);
 	else
 		return (0);
