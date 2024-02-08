a6a9197d7f0d99cb5c8ef6bc6923711a660237d3
DrissArezki <driss.arezki5@gmail.com>
2024-02-08 12:50:25 -0800

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,25 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int a = 0;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
-		a <<= 1;
-		a = a + b[i] - '0';
+		if (b[i] == '0')
+		{
+			a <<= 1;
+		}
+		elif (b[i] == '1')
+		{
+			a = (a << 1) | 1;
+		}
+		else
+		{
+			return (0);
+		}
 		i++;
 	}
 	return (a);
