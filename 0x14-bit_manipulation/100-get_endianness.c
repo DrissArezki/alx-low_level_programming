9621dc66d8b9861bb61927bbbb018805b1d3e3ad
DrissArezki <driss.arezki5@gmail.com>
2024-02-09 14:45:28 -0800

    0x14-bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,14 +6,6 @@
  ** Return: Always 0.
  **/
 
-/*int get_endianness(void)
-{
-	unsigned int num = 1;
-	unsigned char *byte_ptr = (unsigned char *)&num;
-	
-	return ((byte_ptr[0] == 1) ? 1 : 0);
-}**/
-
 int get_endianness(void)
 {
 	int a;
