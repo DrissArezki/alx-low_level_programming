e0b15c682e930c47765323ae108d223234aab516
DrissArezki <driss.arezki5@gmail.com>
2024-02-12 15:42:27 -0800

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,37 +12,37 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buff;
 	ssize_t bytes;
 	ssize_t i;
-	
+
 	if (!filename)
 		return (0);
-	
+
 	f = open(filename, O_RDONLY, 0777);
-	
+
 	if (f == -1)
 		return (0);
-	
+
 	buff = malloc(letters * sizeof(char));
-	
+
 	if (!buff)
 		return (0);
-	
+
 	bytes = read(f, buff, letters);
-	
+
 	if (bytes == -1)
 	{
 		free(buff);
 		return (0);
 	}
-	
+
 	for (i = 0; i < bytes; i++)
 	{
 		write(STDOUT_FILENO, &buff[i], 1);
 	}
-	
+
 	if (close(f) == -1)
 		return (0);
-	
+
 	free(buff);
-	
+
 	return (bytes);
 }
